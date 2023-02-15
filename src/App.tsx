6ed28f65b2ea4af481dc2131e91ac2dedd7ac93f
sirpuffin1 <angelcruz981@yahoo.com>
2023-02-15 11:35:36 -0800

    feat(posts): use mutation to create post

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,57 @@
-import './App.css'
-import { useQuery, useMutation } from '@tanstack/react-query'
-
-const POSTS = [
-  {id: 1, title: "Post 1"},
-  {id: 2, title: "Post 2"}
-]
+import "./App.css";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+interface IPost {
+  id: number | string;
+  title: string;
+}
+const POSTS: IPost[] = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const queryClient = useQueryClient()
+
   const postsQuery = useQuery({
     queryKey: ["posts"],
-    queryFn: () => wait(1000).then(() => [...POSTS])
-  })
-  const { data, isLoading, isError, error } = postsQuery
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
 
-  if(isLoading) return <h1>Loading...</h1>
-  if(isError) return <pre>{JSON.stringify(error)}</pre>
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title: title })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    }
+  });
 
-  return <div>
-    {data.map((post) => (
-      <>
-      <h1>{post.id}</h1>
-      <p>{post.title}</p>
-      </>
-    ))}
-  </div>
+  const { data, isLoading, isError, error } = postsQuery;
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <pre>{JSON.stringify(error)}</pre>;
+
+  return (
+    <div>
+      {data.map((post) => (
+        <>
+          <h1>{post.id}</h1>
+          <p>{post.title}</p>
+        </>
+      ))}
+   
+      <button 
+      disabled={newPostMutation.isLoading}
+      onClick={() => newPostMutation.mutate("New post")}>Click me</button>
+    </div>
+  );
 }
 
 // function to simulate load times
 function wait(duration: number) {
-  return new Promise(resolve => setTimeout(resolve, duration))
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
-export default App
+export default App;
