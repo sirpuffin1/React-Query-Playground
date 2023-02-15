5b436cb26b276010db42051097e975eb79e9e19b
sirpuffin1 <angelcruz981@yahoo.com>
2023-02-15 10:50:40 -0800

    feat(posts): create/map through posts query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css'
+import { useQuery, useMutation } from '@tanstack/react-query'
+
+const POSTS = [
+  {id: 1, title: "Post 1"},
+  {id: 2, title: "Post 2"}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React Query</h1>
-    </div>
-  )
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+  const { data, isLoading, isError, error } = postsQuery
+
+  if(isLoading) return <h1>Loading...</h1>
+  if(isError) return <pre>{JSON.stringify(error)}</pre>
+
+  return <div>
+    {data.map((post) => (
+      <>
+      <h1>{post.id}</h1>
+      <p>{post.title}</p>
+      </>
+    ))}
+  </div>
 }
 
 // function to simulate load times
